2ffdf9478d41c06b84694a68a19629eee25daa08
emilio-quintana-dev <emilioquintana90@gmail.com>
2020-07-27 15:44:48 -0500

    added end points to update notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,20 @@
 class NotesController < ApplicationController
 
+    def show
+        note = Note.find(params[:id])
+
+        if note
+            render json: {
+                status: "Success",
+                note: note
+            }
+        else
+            render json: {
+                status: 401
+            }
+        end
+    end
+
     def destroy
         note = Note.find(params[:id])
         note.destroy
@@ -22,5 +37,36 @@ class NotesController < ApplicationController
         else
             render json: { status: 401 }
         end
+    end
+
+    def update
+        note = Note.find(params[:id])
+
+        if note
+            if params[:title]
+                note.update(title: params[:title])
+            end
+
+            if params[:description]
+                note.update(description: params[:description])
+            end
+
+            if params[:done]
+                note.update(done: params[:done])
+            end
+
+            note.save
+
+            render json: {
+                status: "SUCCESS",
+                note: note
+            }
+        else
+            render json: {
+                status: "ERROR"
+            }
+            
+        end
+            
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'users/:id/notes', to: "users#notes"
+  get 'notes/:id', to: "notes#show"
   delete 'notes/:id', to: "notes#destroy"
   post 'notes/new', to: "notes#create"
+  patch 'notes/:id', to: "notes#update"
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
  
